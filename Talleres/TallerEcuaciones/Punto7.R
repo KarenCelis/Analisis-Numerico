5bc86e0450f34a32bbee3ebe708bc1cfc45cdd1c
Karen Celis <47069950+KarenCelis2@users.noreply.github.com>
2019-02-27 23:55:12 -0500

    Add files via upload

--- Talleres/TallerEcuaciones/Punto7.R
+++ Talleres/TallerEcuaciones/Punto7.R
@@ -1,8 +1,8 @@
 #Barzilai-Borwein se usa para resolver el sistema no lineal de ecuaciones, y para optimizar las funciones objetivas no lineales sujetas a restricciones simples. Es especialmente útil en problemas donde los algoritmos son propensos a experimentar dificultades en la convergencia.
 ```{r, echo=T}
 trigexp = function(x) {
-  n = length(x)
-  F = rep(NA, n)
+  n = length(x)#n es el tamaño de x
+  F = rep(NA, n)#replica los valores
   F[1] = 3*x[1]^2 + 2*x[2] - 5 + sin(x[1] - x[2]) * sin(x[1] + x[2])
   tn1 = 2:(n-1)
   F[tn1] = -x[tn1-1] * exp(x[tn1-1] - x[tn1]) + x[tn1] *
